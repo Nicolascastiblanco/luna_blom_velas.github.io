0e12aacb6f27a9e1dc1c152cdc094629b3332d81
nicolas <ni-vanico@hotmail.co>
2026-02-19 20:47:41 -0500

    Update base path to root for GitHub Pages user site

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/luna_blom_velas.github.io/",
+  base: "/",
   publicDir: "public",
   build: {
     outDir: "dist",
